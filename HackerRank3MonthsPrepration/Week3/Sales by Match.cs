65cf7ab6c3160d0a0360bd8be7f80ad9e179e8cc
alpersarac <alper.sarac42@gmail.com>
2023-08-16 19:56:46 +0300

    Sales by Match

---
+++ HackerRank3MonthsPrepration/Week3/Sales by Match.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank3MonthsPrepration.Week3
+{
+    public class Sales_by_Match
+    {
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int totalPairCount = 0;
+            var coloursDistincted = ar.Distinct().ToList();
+
+            foreach (var colour in coloursDistincted)
+            {
+                var ww= ar.Where(x => x == colour).Count() / 2;
+            }
+            return totalPairCount;
+        }
+    }
+}
