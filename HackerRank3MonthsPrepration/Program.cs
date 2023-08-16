062ef9fc7a5e0367eb60433c464e4e3f812b776a
alpersarac <alper.sarac42@gmail.com>
2023-08-16 20:40:45 +0300

    Migratory Birds

--- HackerRank3MonthsPrepration/Program.cs
+++ HackerRank3MonthsPrepration/Program.cs
@@ -14,5 +14,6 @@ namespace HackerRank3MonthsPrepration
         {
             
         }
+        
     }
 }
---
+++ HackerRank3MonthsPrepration/Week3/Migratory Birds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank3MonthsPrepration.Week3
+{
+    internal class Migratory_Birds
+    {
+        public static int migratoryBirds()
+        {
+            var arr = new List<int> { 1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4 };
+            var lowerSightedBird = arr.GroupBy(x => x).OrderByDescending(x => x.Count()).Select(x => x.Key).ToList().FirstOrDefault();
+
+            return lowerSightedBird;
+        }
+    }
+}
